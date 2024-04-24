f5b35065744f96673392f8072a053dd7029648f4
sujunwe <1011232378@qq.com>
2024-04-24 22:20:42 +0800

    add third party

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "process.h"
+#include "show_result.h"
+
 using std::cout, std::endl;
 
 
@@ -10,5 +12,9 @@ int main()
     pro.planProcess();
     cout << "planning end" << endl;
 
+    cout << "show result: " << endl;
+    ShowResult show;
+    //show.drawResult();
+    
     return 0;
 }
--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "process.h"
+#include <Eigen/Dense>
+
 using std::cout, std::endl;
+using Eigen::MatrixXd;
 
 void Process::planProcess()
 {
     cout << "this is planing process" << endl;
     my_map.mapInfo();
     cout << "planning success" << endl;
+
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 5;
+    m(0, 1) = -4;
+    m(1, 1) = 8;
+
+    cout << m << endl;
 }
